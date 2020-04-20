0deaa338cbd59dc4ceb1c800c9a8d44d3994137c
FreeLife <380398082@qq.com>
2020-04-20 14:16:14 +0800

    项目初始化结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// 注册路由
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// 配置 路由规则
+const routes = []
 
 const router = new VueRouter({
   routes
