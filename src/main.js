9cf2ab3b5fc5015b26ddf4da6c83e475fa7ab0d5
FreeLife <380398082@qq.com>
2020-04-20 14:25:17 +0800

    导入Element组件库

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// 加载 element 组件库
+import ElementUI from 'element-ui'
+
+// 加载 element 组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
 // 加载全局样式
 import './styles/index.less'
 
+// 注册 Element 组件库
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
+// 创建 Vue 根实例
 new Vue({
   router,
   render: h => h(App)
