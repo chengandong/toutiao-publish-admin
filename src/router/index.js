fb0b7f09aea44341352a98f0a3fc2d58684ef497
FreeLife <380398082@qq.com>
2020-04-21 22:52:25 +0800

    后台页面-layout页面结构布局

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,10 @@ import VueRouter from 'vue-router'
 
 // 导入 登录页面 组件
 import Login from '@/views/login'
+
 // 导入 首页页面 组件
 import Home from '@/views/home'
+import Layout from '@/views/layout'
 // 注册路由
 Vue.use(VueRouter)
 
@@ -16,9 +18,16 @@ const routes = [
     component: Login
   },
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/', // path 为空，会默认由子路由渲染
+    // name: 'layout', //如果有默认子路由，就不要给父路由起名字了
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: Home
+      }
+    ]
   }
 ]
 
---
+++ src/views/layout/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-container class="layout-container">
+      <el-aside
+      class="aside"
+      width="200px"
+      >Aside</el-aside>
+      <el-container>
+        <el-header class="header">Header</el-header>
+        <el-main class="main">
+            <!-- 子路由 渲染 -->
+            <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex'
+}
+</script>
+
+<style scoped lang="less">
+.layout-container {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    .aside {
+        background-color: #002033;
+    }
+    .header {
+        border-bottom: 1px solid #ccc;
+    }
+}
+</style>
