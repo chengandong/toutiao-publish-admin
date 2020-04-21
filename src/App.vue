1ea6fd7627c130a2dc83c4b64250b3992eebd0f1
FreeLife <380398082@qq.com>
2020-04-21 09:03:21 +0800

    用户登录-组件的创建与配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <h2>黑马头条发布内容</h2>
     <!-- 路由对应的组件渲染 -->
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 导入 登录页面 组件
+import Login from '@/views/login'
+
 // 注册路由
 Vue.use(VueRouter)
 
 // 配置 路由规则
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
+]
 
 const router = new VueRouter({
   routes
