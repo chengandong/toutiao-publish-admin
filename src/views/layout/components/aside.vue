2aeda5eca36fe5206b6cff7a04ad042be5c9e9f7
FreeLife <380398082@qq.com>
2020-04-22 20:41:49 +0800

    后台页面-侧边栏图标设置

--- src/views/layout/components/aside.vue
+++ src/views/layout/components/aside.vue
@@ -10,27 +10,27 @@
       <img src="./logo_admin.png" alt="">
     </div>
     <el-menu-item index="/">
-        <i class="el-icon-location"></i>
-        <span slot="title">内容</span>
+        <i class="el-icon-s-home"></i>
+        <span slot="title">首页</span>
     </el-menu-item>
     <el-menu-item index="/article">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-document"></i>
       <span slot="title">内容管理</span>
     </el-menu-item>
     <el-menu-item index="/image">
-      <i class="el-icon-setting"></i>
+      <i class="el-icon-picture"></i>
       <span slot="title">素材管理</span>
     </el-menu-item>
     <el-menu-item index="/publish">
-      <i class="el-icon-setting"></i>
+      <i class="el-icon-s-promotion"></i>
       <span slot="title">发布文章</span>
     </el-menu-item>
     <el-menu-item index="/comment">
-      <i class="el-icon-setting"></i>
+      <i class="el-icon-chat-dot-square"></i>
       <span slot="title">评论管理</span>
     </el-menu-item>
     <el-menu-item index="/fans">
-      <i class="el-icon-setting"></i>
+      <i class="el-icon-user-solid"></i>
       <span slot="title">粉丝管理</span>
     </el-menu-item>
     <el-menu-item index="/setting">
