a920185289dee20be95703f382196c3c5ddc5769
FreeLife <380398082@qq.com>
2020-04-23 14:27:03 +0800

    内容管理-页面布局的实现

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,10 +1,116 @@
 <template>
-  <div class="article-container">内容管理</div>
+  <div class="article-container">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 表单数据 筛选  -->
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="状态">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="全部"></el-radio>
+          <el-radio label="草稿"></el-radio>
+          <el-radio label="待审核"></el-radio>
+          <el-radio label="审核通过"></el-radio>
+          <el-radio label="审核失败"></el-radio>
+          <el-radio label="已删除"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道">
+        <el-select v-model="form.region" placeholder="请选择频道">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="活动时间">
+        <el-date-picker
+          v-model="form.date1"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">筛选</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- 表格 数据列表 -->
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="封面"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="标题"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="状态">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="发布时间">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
+    <!-- 列表分页 -->
+    <el-pagination
+      layout="prev, pager, next"
+      background
+      :total="1000">
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ArticleIndex'
+  name: 'ArticleIndex',
+  data () {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
