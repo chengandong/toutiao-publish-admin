59284aa2d44dd19696d42e3dc25b19746cc212a7
FreeLife <380398082@qq.com>
2020-04-24 17:41:50 +0800

    内容管理-表格数据列表状态列的 优化处理

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -65,11 +65,12 @@
           label="状态">
           <!-- 自定义模板 -->
           <template slot-scope="scope">
-            <el-tag v-if="scope.row.status === 0" type="warning">草稿</el-tag>
+            <el-tag :type="articleStatus[scope.row.status].type">{{articleStatus[scope.row.status].text}}</el-tag>
+            <!-- <el-tag v-if="scope.row.status === 0" type="warning">草稿</el-tag>
             <el-tag v-else-if="scope.row.status === 1">待审核</el-tag>
             <el-tag v-else-if="scope.row.status === 2" type="success">审核通过</el-tag>
             <el-tag v-else-if="scope.row.status === 3" type="danger">审核失败</el-tag>
-            <el-tag v-else-if="scope.row.status === 4" type="info">已删除</el-tag>
+            <el-tag v-else-if="scope.row.status === 4" type="info">已删除</el-tag> -->
           </template>
         </el-table-column>
         <el-table-column
@@ -121,7 +122,14 @@ export default {
         resource: '',
         desc: ''
       },
-      articlesData: [] // 文章数据列表
+      articlesData: [], // 文章数据列表
+      articleStatus: [ // 文章数据列表 状态
+        { status: 0, text: '草稿', type: 'warning' },
+        { status: 1, text: '待审核', type: '' },
+        { status: 2, text: '审核通过', type: 'success' },
+        { status: 3, text: '审核失败', type: 'danger' },
+        { status: 4, text: '已删除', type: 'info' }
+      ]
     }
   },
   created () {
