2630ac374dffbacb917a27f7ff24e449ea00197c
FreeLife <380398082@qq.com>
2020-04-25 17:34:33 +0800

    发布文章-发布以及存为草稿功能的实现

--- src/api/article.js
+++ src/api/article.js
@@ -26,3 +26,15 @@ export const deleteArticle = articleId => {
     url: `/mp/v1_0/articles/${articleId}`
   })
 }
+
+// 发表 文章
+export const publishArticle = (data, draft = false) => {
+  return request({
+    method: 'POST',
+    url: '/mp/v1_0/articles',
+    data,
+    params: {
+      draft // 是否存为草稿（true 为草稿）
+    }
+  })
+}
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,30 +9,34 @@
       </el-breadcrumb>
     </div>
     <!-- Form 表单区域 -->
-    <el-form ref="form" :model="form" label-width="60px">
+    <el-form ref="form" :model="article" label-width="60px">
       <el-form-item label="标题">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="article.title"></el-input>
       </el-form-item>
       <el-form-item label="内容">
-        <el-input type="textarea" v-model="form.desc"></el-input>
+        <el-input type="textarea" v-model="article.content"></el-input>
       </el-form-item>
       <el-form-item label="封面">
-        <el-radio-group v-model="form.resource">
-        <el-radio label="单图"></el-radio>
-        <el-radio label="三图"></el-radio>
-        <el-radio label="无图"></el-radio>
-        <el-radio label="自动"></el-radio>
+        <el-radio-group v-model="article.cover.type">
+        <el-radio :label="1">单图</el-radio>
+        <el-radio :label="3">三图</el-radio>
+        <el-radio :label="0">无图</el-radio>
+        <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
+        <el-select v-model="article.channel_id" placeholder="请选择活动区域">
+        <el-option
+        v-for="channel in channels"
+        :key="channel.id"
+        :label="channel.name"
+        :value="channel.id"
+        ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">发表</el-button>
-        <el-button>存为草稿</el-button>
+        <el-button type="primary" @click="onPublish(false)">发表</el-button>
+        <el-button @click="onPublish(true)">存为草稿</el-button>
       </el-form-item>
     </el-form>
 </el-card>
@@ -40,25 +44,51 @@
 </template>
 
 <script>
+import {
+  getArticleChannels,
+  publishArticle
+} from '@/api/article'
 export default {
   name: 'PublishIndex',
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      }
+      article: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: { // 封面
+          type: 0, // 封面类型
+          images: []
+        },
+        channel_id: null // 文章所属频道id
+      },
+      channels: [] // 文章频道信息
     }
   },
+  created () {
+    this.loadChannels()
+  },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    // 获取 文章频道信息
+    loadChannels () {
+      getArticleChannels().then(res => {
+        // console.log(res)
+        this.channels = res.data.data.channels
+      })
+    },
+    // 发表 文章
+    onPublish (draft) {
+      publishArticle(this.article, draft).then(res => {
+        // console.log(res)
+        // Message 消息提示
+        this.$message({
+          message: `恭喜你,${draft ? '存为草稿' : '发表'}文章信息成功`,
+          type: 'success'
+        })
+        // 跳转到 内容管理 页面
+        this.$router.push('/article')
+      }).catch(err => {
+        console.log(err)
+      })
     }
   }
 }
