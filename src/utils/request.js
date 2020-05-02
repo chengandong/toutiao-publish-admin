feb9c09c2e0256eda1b613a6b1023ce94ce139e9
FreeLife <380398082@qq.com>
2020-05-02 22:41:31 +0800

    页面功能处理公共错误

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,10 @@
 // axios 封装
 import axios from 'axios'
 import jsonBigint from 'json-bigint'
+import router from '@/router/'
 
+// 单独引用
+import { Message } from 'element-ui'
 // 创建一个axios实例 进行配置
 const request = axios.create({
   // 基准地址 配置
@@ -42,6 +45,33 @@ request.interceptors.request.use(
     return Promise.reject(error)
   }
 )
+
+// 响应拦截器
+// Add a response interceptor
+request.interceptors.response.use(function (response) {
+  return response
+}, function (error) {
+  // console.dir(error)
+  const { status } = error.response
+  if (status === 401) {
+    // 清除 本地 存储的用户信息
+    window.localStorage.removeItem('user')
+    // 跳转到 登录页面
+    router.push('/login')
+    // 消息 提示
+    Message({
+      type: error,
+      message: '登录状态无效，请重新登录'
+    })
+  } else if (status === 403) {
+    Message.warning('Token为携带或已经过期')
+  } else if (status === 400) {
+    // 客户端参数错误
+    Message.error('请求参数错误')
+  } else if (status >= 500) {
+    Message.error('服务端内部异常，请稍后重试')
+  }
+})
 
 // 导出 请求包
 export default request
